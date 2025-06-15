d1ab67f9d6264d2b647f39813c94fa124c9abd59
Camila Palma <capalmasoto@gmail.com>
2025-06-15 14:23:10 -0400

    base project

---
+++ src/main/java/com/palma/digidata/DigidataApplication.java
@@ -0,0 +1,13 @@
+package com.palma.digidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigidataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigidataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palma/digidata/DigidataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palma.digidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigidataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
